9cc23e36b1db668c07a927a3286e4c6e2d1f110a
Maschetti <mateusvmaschietto@gmail.com>
2025-02-02 00:19:09 -0300

    chore: add lucide react icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,11 @@
 import '@/styles/global.css'
 import { Slot } from 'expo-router'
+import { View } from 'react-native'
 
 export default function RootLayout() {
-  return <Slot />
+  return (
+    <View className='bg-slate-50 flex-1 items-center justify-center'>
+      <Slot />
+    </View>
+  )
 }
