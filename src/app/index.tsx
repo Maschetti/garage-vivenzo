c98c2737518355df60c51be5a8390ff6e07ab9ef
Maschetti <mateusvmaschietto@gmail.com>
2025-02-04 02:08:18 -0300

    Chore: call created components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import AddCarModal from '@/components/AddCarModal';
+import CarList from '@/components/CarList';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
 
 export default function Index() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <View className='bg-slate-600'>
-      <Text>Hello</Text>
-    </View>
+    <SafeAreaView className='flex-1 p-2'>
+
+      <CarList />
+      
+      <TouchableOpacity
+        className="bg-green-600 p-4 rounded-lg items-center"
+        onPress={() => setIsModalOpen(true)}
+      >
+        <Text className="text-white text-lg font-bold">Check-in de Veículo</Text>
+      </TouchableOpacity>
+
+      <AddCarModal isModalVisible={isModalOpen} closeModal={() => setIsModalOpen(false)} />
+    </SafeAreaView>
   )
 }
