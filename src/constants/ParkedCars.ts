6030026e7406925ff7b14ef073205cdc583032eb
Maschetti <mateusvmaschietto@gmail.com>
2025-02-06 15:20:19 -0300

    fix: remove identifier from Parked Cars

--- src/constants/ParkedCars.ts
+++ src/constants/ParkedCars.ts
@@ -1,8 +1,8 @@
 export const ParkedCars = [
-  { identifier: 45, number: "DEF-9101", floor: "3", spot: '6'},
-  { identifier: 12, number: "MNO-5566", floor: "2", spot: '15'},
-  { identifier: 67, number: "VWX-2233", floor: "Térreo", spot: '1'},
-  { identifier: 28, number: "EFG-8899", floor: "4", spot: '6'},
-  { identifier: 59, number: "NOP-5567", floor: "2", spot: '5'},
-  { identifier: 81, number: "WXY-1123", floor: "3", spot: '2'}
+  { number: "DEF-9101", floor: "3", spot: '6'},
+  { number: "MNO-5566", floor: "2", spot: '15'},
+  { number: "VWX-2233", floor: "Térreo", spot: '1'},
+  { number: "EFG-8899", floor: "4", spot: '6'},
+  { number: "NOP-5567", floor: "2", spot: '5'},
+  { number: "WXY-1123", floor: "3", spot: '2'}
 ];
